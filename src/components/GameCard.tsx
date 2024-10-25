d17826a1ea859fb05f87459e2168f251b4d6b751
Haileyesus Chekol <teamdevs7@gmail.com>
2024-10-26 00:35:09 +0100

    Displaying score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react"
 import { Game } from "../hooks/useFetch"
 import PlatformIconLsit from "./PlatformIconLsit"
+import CriticScore from "./CriticScore"
 interface Props {
   game: Game
 }
@@ -10,9 +11,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconLsit
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconLsit
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   )
